b8ba160d116b3245448e3f6b5412150a07edbb50
61524 <unconfigured@null.spigotmc.org>
2021-12-13 13:58:28 -0600

    moved content from script.js to scriptHelper.js

--- scriptHelper.js
+++ scriptHelper.js
@@ -24,18 +24,66 @@ function validateInput(testInput) {
    }
 }
 
-function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   let allInputsValid = validateInput(document.getElementById("pilotName").value) === "Not a Number" &&
-                        validateInput(document.getElementById("copilotName").value) === "Not a Number" &&
-                        validateInput(document.getElementById("fuelLevel").value) === "Is a Number" &&
-                        validateInput(document.getElementById("cargoMass").value) === "Is a Number";
-    return allInputsValid;
+function formSubmission(document, list = document.getElementById("faultyItems"),
+                         pilot = document.getElementById("pilotName").value,
+                         copilot = document.getElementById("copilotName").value,
+                         fuelLevel = document.getElementById("fuelLevel").value,
+                         cargoLevel = document.getElementById("cargoMass").value) {
+
+    let allInputsValid = validateInput(pilot) === "Not a Number" &&
+                        validateInput(copilot) === "Not a Number" &&
+                        validateInput(fuelLevel) === "Is a Number" &&
+                        validateInput(cargoLevel) === "Is a Number";
+    let buttonValues = [pilot, copilot, fuelLevel, cargoLevel];
+    let fieldBlank = false;
+    let launchReady = true;
+    for (let i = 0; i < buttonValues.length; i++) {
+        if (validateInput(buttonValues[i]) === "Empty") {
+            fieldBlank = true;
+            launchReady = false;
+        }
+    }
+    if (fieldBlank) {
+        alert("All fields are required.");
+    } else if (!allInputsValid) {
+        alert("Invalid input.")
+    } else {
+        document.getElementById("pilotStatus").textContent = `Pilot ${buttonValues[0]} is ready for launch`;
+        document.getElementById("copilotStatus").textContent = `Co-pilot ${buttonValues[1]} is ready for launch`;
+
+        if (buttonValues[2] < 10000) {
+            list.style.visibility = "visible";
+            document.getElementById("fuelStatus").textContent = `Fuel level too low for launch`;
+            document.getElementById("launchStatus").textContent = "Shuttle Not Ready for Launch";
+            document.getElementById("launchStatus").style.color = "rgb(199, 37, 78)"; //red
+            launchReady = false;
+        } else {
+            document.getElementById("fuelStatus").textContent = `Fuel level high enough for launch`;
+        }
+
+        if (buttonValues[3] > 10000) {
+            list.style.visibility = "visible";
+            document.getElementById("cargoStatus").textContent = `Cargo mass too heavy for launch`;
+            document.getElementById("launchStatus").textContent = "Shuttle Not Ready for Launch";
+            document.getElementById("launchStatus").style.color = "rgb(199, 37, 78)"; //red
+            launchReady = false;
+        } else {
+            document.getElementById("cargoStatus").textContent = `Cargo mass low enough for launch`;
+        }
+        if (launchReady) {
+            list.style.visibility = "visible";
+            document.getElementById("launchStatus").textContent = "Shuttle is Ready for Launch";
+            document.getElementById("launchStatus").style.color = "rgb(65, 159, 106)"; //green
+        }
+    }
 }
 
 async function myFetch() {
     let planetsReturned;
     planetsReturned = await fetch("https://handlers.education.launchcode.org/static/planets.json");
+    planetsReturned = await planetsReturned.json();
     return planetsReturned;
+    
 }
 
 function pickPlanet(planets) {
